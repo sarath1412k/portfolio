0c537d7a22d9ad1dd5621ed05d0ac84cab3494f8
sarath1412k <161072592+sarath1412k@users.noreply.github.com>
2024-11-23 14:11:56 +0530

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { Collapse, Container, Nav, Navbar, NavbarToggler, NavItem, NavLink } from "reactstrap"
 import './styles.css'
 const Layout: React.FC<any> = ({ children }) => {

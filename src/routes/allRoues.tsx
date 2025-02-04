47aa459b614790c01136c2f0c32efd8ce3cf6ab9
sarathkumaraara <165144755+sarathkumaraara@users.noreply.github.com>
2025-02-04 12:15:25 +0530

    scroll routes
    scroll routes

--- src/routes/allRoues.tsx
+++ src/routes/allRoues.tsx
@@ -1,9 +1,35 @@
+import React from "react";
 import HomePage from "../pages/HomePage";
 import LearningSkills from "../pages/LearingSkills";
 import PageNotFound from "../pages/PageNotFound";
 import Projects from "../pages/Projects";
 import Skills from "../pages/Skills";
 import { Navigate } from "react-router-dom";
+
+const scrollableRoutes = () => {
+
+  const routeData = [
+    {key:"",component:<HomePage/>},
+    {key:"",component:<Skills/>},
+    {key:"",component:<Projects/>},
+    {key:"",component:<LearningSkills/>},
+    {key:"",component:<HomePage/>},
+  ]
+  return (
+    <React.Fragment>
+      {
+        routeData.map((data:any) => {
+          return (
+            <div key={data.key} id={data.id} data-section={data.key}>
+              {data.component}
+            </div>
+          )
+        })
+      }
+    </React.Fragment>
+  )
+}
+
 export const publicRoutes = [
   { path: "/", component: <Navigate to="/home" replace /> },
   { path: "/home", component: <HomePage />, index: true },
